64b5b4b7f75edeed6571f3959cdc8c2066665b7f
Gia Jennings <gjennings.co@gmail.com>
2021-05-02 01:50:00 -0400

    Add validations for user and run models

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,3 +1,6 @@
 class Run < ActiveRecord::Base
     belongs_to :user
+    validates :date, presence: true
+    validates :distance, presence: true
+    validates :duration, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :runs
+    validates :username, uniqueness: true, length: {in: 5..10}
+    validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :password, length: {in: 8..100}, confirmation: true
 end
