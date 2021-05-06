0a5c6f5882b2424229e8176ef2a2c1a05c30b0a8
Gia Jennings <gjennings.co@gmail.com>
2021-05-05 21:23:32 -0400

    Remove show.html.erb from users view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :runs
+    has_many :runs, class_name: "Run", foreign_key: "user_id" 
     validates :username, uniqueness: true, length: {in: 5..10}
     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
     validates :password, length: {in: 8..100}
