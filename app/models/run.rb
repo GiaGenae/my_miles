5e87f9317e4915e54e5084e3074a76edcfd8282c
Gia Jennings <gjennings.co@gmail.com>
2021-05-10 12:38:04 -0400

    Add self validation for params

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,4 +3,8 @@ class Run < ActiveRecord::Base
     validates :date, presence: true
     validates :distance, presence: true
     validates :duration, presence: true
+
+    def self.valid_params?(params)
+        return !params[:date].empty? && !params[:distance].empty? && !params[:duration].empty?
+    end
 end
