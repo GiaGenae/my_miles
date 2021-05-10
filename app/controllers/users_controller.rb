0f70a831875c546b6934a7b67d2cc7b10649b573
Gia Jennings <gjennings.co@gmail.com>
2021-05-10 12:37:12 -0400

    Recode users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,50 @@
 class UsersController < ApplicationController
-
-  get "/users" do
-    erb :"/users/index.html"
-  end
-
-  get "/users/signup" do
-    erb :"/users/signup.html"
-  end
-
-  post "/users/signup" do
-    user = User.create(params["user"])
-    if user.valid? 
-      session[:id] = user.id
-      redirect '/runs/new'
+  get '/signup' do
+    if !logged_in?
+      erb :"users/new"
     else
-      flash[:error] = user.errors.full_messages.first
-      redirect '/users/signup'
+      redirect "/runs"
     end
   end
 
-  get "/login" do
-    redirect "/runs" if logged_in?
-    erb :"/users/login.html"
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save && !params[:username].empty?
+      @user.save
+      session[:user_id] = @user.id
+      redirect '/runs'
+    else
+      flash[:error] = "Invalid entry."
+      redirect '/signup'
+    end
   end
 
-  post "/login" do
-    user = User.find_by_username(params["user"]["username"])
-    if user && user.authenticate(params["user"]["password"])
-      session[:id] = user.id
+  get '/login' do
+    if logged_in?
       redirect "/runs"
+    else 
+      erb :"/users/login"
+    end
+  end
+  
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/runs'
     else
-      flash[:error] = "Invalid credentials."
-      redirect "/login"
+      redirect '/signup'
     end
   end
 
   get "/logout" do
     session.clear
-    redirect "/"
+    redirect "/login"
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    @runs =  @user.runs
+    erb :'/users/show'
   end
 end
