9a2d83068e195d43db6760b04e920edaff95f318
Gia Jennings <gjennings.co@gmail.com>
2021-05-10 12:36:07 -0400

    Update runs controller

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,51 +1,39 @@
 class RunsController < ApplicationController
-
-  # before ("/runs") do
-  #   redirect_if_not_logged_in if request.path_info != "/login"
-  # end
-
+  
   get '/runs' do
     if logged_in?
       @user = current_user
       @runs = current_user.runs 
-      erb:"runs/index.html"
+      erb:"runs/index"
     else
+      redirect_if_not_logged_in
       redirect "/login"
     end
   end
 
   get '/runs/new' do
+    redirect_if_not_logged_in
     if logged_in?
-      erb :'/runs/new.html'
+      erb :'/runs/new'
     else
       redirect "/login"
     end
   end
 
   post '/runs' do
-    run = current_user.runs.create(date: params[:run][:date],distance: params[:run][:distance],duration: params[:run][:duration])
-    if run.valid?
+    @s = current_user.runs.build(params)
+    if @s.save
       redirect "/runs"
     else
-      flash[:error] = run.errors.full_messages.join(", ")
-      redirect "/runs/new"
+      erb :"runs/new"
     end
   end
 
-  get '/runs/:id' do
-    @run = Run.find_by(id: params[:id])
-    if logged_in?
-      erb :"/runs/show.html"
-    else
-      redirect "/login"
-    end
-  end 
-  
   get '/runs/:id/edit' do
     if logged_in?
       @run = Run.find_by_id(params[:id])
       if @run.user_id == current_user.id
-        erb :'runs/edit.html'
+        erb :'runs/edit'
       else
         redirect "/runs"
       end
@@ -59,9 +47,18 @@ class RunsController < ApplicationController
     unless Run.valid_params?(params)
       redirect "/runs/#{@run.id}/edit?error=invalid run"
     end
-    @run.update(params.select{|k|k=="date" || k=="distance" || k=="duration"})
-    redirect "/runs/index.html"
+    @run.update(params.select{|k|k=="distance" || k=="duration" || k=="date"})
+    redirect "/runs/#{@run.id}"
   end
+
+  get '/runs/:id' do
+    @run = Run.find_by(id: params[:id])
+    if logged_in?
+      erb :"/runs/show"
+    else
+      redirect "/login"
+    end
+  end 
 
   delete '/runs/:id' do
     @run = Run.find(params[:id])
@@ -72,5 +69,6 @@ class RunsController < ApplicationController
       redirect "/login"
     end
   end
+
 
 end
